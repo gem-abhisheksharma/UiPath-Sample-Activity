82641bb3bebe903ee50b6709b654291678d27257
gem-abhisheksharma <abhishek.sharma@geminisolutions.in>
2022-09-16 14:21:05 +0530

    Add project files.

---
+++ MathOperations/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace MathOperations
+{
+    public class SimpleFormula : CodeActivity
+    {
+        [Category("Input")]
+        [DisplayName("First Number")]
+        [Description("First Number")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        [DisplayName("Second Number")]
+        [Description("Second Number")]
+        [RequiredArgument]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        [DisplayName("Result")]
+        [Description("Sum of First and Second Number")]
+        public OutArgument<double> ResultNumber { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = firstNumber + secondNumber;
+            ResultNumber.Set(context, result);
+        }
+    }
+}
---
+++ MathOperations/DesignerMetadata.cs
@@ -0,0 +1,17 @@
+﻿using MathOperations;
+using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+
+
+namespace ActMathOperations
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            AttributeTableBuilder attributeTableBuilder = new AttributeTableBuilder();
+            attributeTableBuilder.AddCustomAttributes(typeof(SimpleFormula), new DesignerAttribute(typeof(ActivityDesigner1)));
+            MetadataStore.AddAttributeTable(attributeTableBuilder.CreateTable());
+        }
+    }
+}
